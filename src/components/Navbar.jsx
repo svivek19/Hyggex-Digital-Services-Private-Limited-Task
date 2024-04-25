80da062b082cc3494d3d15c462f2fc3e1ef1b55a
svivek19 <svivek030503@gmail.com>
2024-04-25 16:32:33 +0530

    navbar finish

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import logo from "../assets/logoBrand.png";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav>
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src={logo} className="h-12" alt=" Logo" />
+        </a>
+        <button
+          onClick={toggleNavbar}
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden "
+          aria-controls="navbar-default"
+          aria-expanded={isOpen ? "true" : "false"}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div
+          className={`w-full md:block md:w-auto ${isOpen ? "" : "hidden"}`}
+          id="navbar-default"
+        >
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-5 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 text-[#3A3740]">
+            <li>Home</li>
+            <li>Flashcard</li>
+            <li>Contact</li>
+            <li>FAQ</li>
+            <li className=" bg-gradient-to-b from-blue-900 to-blue-500 text-white px-5 py-1 rounded-full">
+              Login
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
