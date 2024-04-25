8a5b929db30ab8e042e65d32cf824d86099dfd0f
svivek19 <svivek030503@gmail.com>
2024-04-25 16:41:02 +0530

    Breadcrumb finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Breadcrumb from "./components/Breadcrumb";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Breadcrumb />
     </>
   );
 };
